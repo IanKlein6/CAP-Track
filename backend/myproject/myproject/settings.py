72ce55c2b375f3ac7d06c96763b93bb6dddaaf32
IanKlein6 <ianklein66@gmail.com>
2024-02-17 19:27:08 +0100

    connected django to mysql

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -9,15 +9,20 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Loader from .evn file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-wapi9*!(^g2q=thr$h8sl9k@8n(4)fz9m76t2b2#j^p20-m4au'
@@ -29,7 +34,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+
+    'timesheet.apps.TimesheetConfig',
 ]
 
 MIDDLEWARE = [
@@ -72,16 +79,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'your_database_name',
-        'USER': 'your_mysql_username',
-        'PASSWORD': 'your_mysql_password',
-        'HOST': 'localhost',   # Or your MySQL host
-        'PORT': '3306',        # Or your MySQL port
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),   # Default to localhost if not set
+        'PORT': os.getenv('DB_PORT', '3306'),        # Default to 3306 if not set
     }
 }
 
--- backend/myproject/timesheet/admin.py
+++ backend/myproject/timesheet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import JobSite, WorkEntry, ConstructionManager
+from .models import JobSite, WorkEntry, ConstructionManager
 
 class JobSiteAdmin(admin.ModelAdmin):
     list_display = ('name', 'construction_manager')
