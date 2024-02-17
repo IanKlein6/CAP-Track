bebd6237bf25b3e878e7b2d62456e885ab62fa04
IanKlein6 <ianklein66@gmail.com>
2024-02-17 15:33:36 +0100

    created models work entry, manager, jobsite

--- backend/myproject/timesheet/admin.py
+++ backend/myproject/timesheet/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from models import JobSite, WorkEntry, ConstructionManager
 
-# Register your models here.
+class JobSiteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'construction_manager')
+
+class WorkdEntryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'job_site', 'date', 'hours_wored', 'calendar_week')
+
+admin.site.register(JobSite)
+admin.site.register(WorkEntry)
+admin.site.register(ConstructionManager)
+
+
+#from django.contrib.auth.models import User  Only needed if customizing the User admin
+# If customizing User admin, you would first unregister, then register with a custom ModelAdmin
+# admin.site.unregister(User)
+# admin.site.register(User, CustomUserAdmin)
--- backend/myproject/timesheet/models.py
+++ backend/myproject/timesheet/models.py
@@ -1,32 +1,67 @@
-from django.db import models
+from django.db import models 
+from django.core.validators import RegexValidator 
 from django.contrib.auth.models import User
-from django.utils import timezone
+from django.utils import timezone 
+from datetime import datetime
 
 
-#class Job(models.Model):
+### Infastructure Models
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Example: ...")
 
-# Soft Delete Manager 
-class ActiveWorkEntryManager(models.Manager):
-    def get_queryset(self):
-        # Use the timestamp to filter for non-deleted entries
+class ActiveObjectsManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
         return super().get_queryset().filter(deleted_at__isnull=True)
     
-class WorkEntry(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+class SoftDeleteModel(models.Model):
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = models.Manager() # Default manager
+    active_objects = ActiveObjectsManager() # Custom manager to filter out soft-delete records
+
+    class Meta:
+        abstract = True # stops creations of seperate table in database    
     
-
-    # Flag softdelete timestamp
-    deleted_at = models.DateTimeField(null=True, blank=True) # Time flag for soft delete
-    # Manager
-    objects = models.Manager() # Default Manager
-    active = ActiveWorkEntryManager() # Custom manager for active entries
-
-
-    # Override delete method
     def delete(self, *args, **kwargs):
-        # Timestamp
         self.deleted_at = timezone.now()
         self.save()
-                            
-    # Use WorkEntry.active.all() to get only the entries that arnet' soft-deleted.
+    
+    def hard_delete(self, *args, **kwargs):
+        super(SoftDeleteModel, self).delete(*args, **kwargs)
+
+
+### Normal Models 
+class ConstructionManager(SoftDeleteModel):
+    name = models.CharField(max_length=225)
+    phone_number = models.CharField(validators=[phone_regex], max_length=13, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class JobSite(SoftDeleteModel):
+    name = models.CharField(max_length=255)
+    construction_manager = models.ForeignKey(ConstructionManager, on_delete=models.CASCADE)
+    #hours total
+
+    def __str__(self):
+        return self.name
+
+class WorkEntry(SoftDeleteModel): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    calendar_week = models.IntegerField(null=True, blank=True)
+    calendar_year = models.IntegerField(null=True, blank=True)
+    def save(self, *args, **kwargs):
+        if self.date:
+            year_week = self.date.isocalendar()
+            self.calendar_year, self.calendar_week = year_week[0], year_week[1]
+        super(WorkEntry, self).save(*args, **kwargs)
+    
+    job_site = models.ForeignKey(JobSite, on_delete=models.CASCADE)
+    hours_worked = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.job_site.name} - {self.date} - {self.calendar_week} - {self.calendar_year}"
+
+
+    
