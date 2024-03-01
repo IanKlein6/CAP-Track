3dbc5cea93fc161c5fec97441c58669ef7b31697
IanKlein6 <ianklein66@gmail.com>
2024-03-01 13:57:16 +0100

    test login

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeTestPage from './pages/HomeTestPage';
 import UserSignUp from './pages/UserSignUp';
+import LogIn from './pages/LogInTest';
 
 // Import other pages as needed
 
@@ -10,9 +11,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="test/" element={<HomeTestPage />} />
-        <Route path="signup/" element={<UserSignUp />} />
-        
+        <Route path="/test" element={<HomeTestPage />} />
+        <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/login" element={<LogIn />} />
         {/* Define other routes as needed */}
       </Routes>
     </Router>
